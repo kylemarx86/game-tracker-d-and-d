d5d5a46c53ecef80ab51192d4452f6a6aac6aeb1
kylemarx86 <kylemarx86@csu.fullerton.edu>
2018-01-30 13:11:05 -0800

    add Games class and tests for it.

---
+++ server/utils/games.js
@@ -0,0 +1,76 @@
+class Games {
+    constructor() {
+        this.games = [];
+    }
+
+    addGame(name) {
+        var games = this.games.filter((game) => game.name === name);
+        if(!games[0]) {
+            var game = {
+                name,
+                userCount: 1
+            }
+            this.games.push(game);
+            this.sortGamesByUserCount();
+        }
+        
+        return game;
+    }
+    
+    addUserToGame(name) {
+        var gameIndex = this.games.findIndex((game) => game.name === name);
+
+        // find index returns index of first matching index of matching criteria or -1 if not found
+        if(gameIndex !== -1){
+            // found game index
+            this.games[gameIndex].userCount++;
+            this.sortGamesByUserCount();
+            return this.games[gameIndex];
+        }else{
+            // did not find game index, create game
+            return this.addGame(name);
+        }
+    }
+
+    removeUserFromGame(name) {
+        var gameIndex = this.games.findIndex((game) => game.name === name);
+
+        // find index returns index of first matching index of matching criteria or -1 if not found
+        if(gameIndex !== -1){
+            // found game index and decrement userCount by one
+            this.games[gameIndex].userCount--;
+            if(this.games[gameIndex].userCount === 0){
+                // removeGame returns a game object
+                return this.removeGame(name);
+            }
+            this.sortGamesByUserCount();
+            // returns a game object
+            return this.games[gameIndex];
+        }
+        // returns the name
+        return name;
+    }
+
+    removeGame(name) {
+        // if the game we are checking has one user then we can delete it
+        var game = this.games.filter((game) => game.name === name)[0];
+
+        if(game){
+            this.games = this.games.filter((game) => game.name !== name);
+        }
+        this.sortGamesByUserCount();
+
+        return game;
+    }
+
+    // sorts the games with highest userCount first
+    sortGamesByUserCount(){
+        this.games.sort(function(a, b){
+            return b.userCount - a.userCount;
+        });
+        return this.games;
+    }
+
+}
+
+module.exports = {Games};
