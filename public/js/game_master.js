ac0cea84a00f11add6fc7754336c254c626f7392
Kyle Marx <kylemarx86@csu.fullerton.edu>
2018-02-15 23:15:26 -0800

    style changes game_master view. add 'add' button with test functionality to items in game_master tables.

--- public/js/game_master.js
+++ public/js/game_master.js
@@ -1,28 +1,28 @@
 var socket = io();
 
-socket.on('connect', function() {
+socket.on('connect', function () {
     console.log('Connected to server');
     // console.log('data', data);
 
-    socket.emit('joinGame', data, function(err) {
-        if(err){
-            alert(err);
-            window.location.href = '/';
-        } else{
-            console.log('no error');
-        }
-    });
+    // socket.emit('joinGame', data, function(err) {
+    //     if(err){
+    //         alert(err);
+    //         window.location.href = '/';
+    //     } else{
+    //         console.log('no error');
+    //     }
+    // });
 
     // socket.on('playerConnecting', function() {
     //     console.log('player trying to connect to game. Allow? Y/N');
     // });
 });
 
-socket.on('disconnect', function() {
+socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 
-socket.on('welcome', function(info) {
+socket.on('welcome', function (info) {
     var $p = $('<p>').text(`welcome ${info.name} to the game.`);
     $('body').append($p);
 });
@@ -38,6 +38,16 @@ socket.on('welcome', function(info) {
 //     // $('#users').html($ol);      // updates / wipes and adds to prevent duplicates
 // });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.collapsible').collapsible();
-  });
+    $('.add-to-cart').click(function () {
+        addToCart(this);
+        // console.log('click')
+    });
+});
+
+function addToCart($item) {
+    console.log('added to cart')
+    var $name = $($item).closest('tr').find('td:nth-of-type(1)').text();
+    console.log($name)
+}
