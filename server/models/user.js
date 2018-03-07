b1e1cc14a1fd8f5af6495d4b3bd5d64243ce580a
Kyle Marx <kylemarx86@csu.fullerton.edu>
2018-03-07 15:52:46 -0800

    basic db setup.

---
+++ server/models/user.js
@@ -0,0 +1,133 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: value => validator.isEmail(value),
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+}, {
+    // settings object
+    usePushEach: true
+});
+
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
+
+    user.tokens.push({access, token});
+    return user.save().then(() => {
+        return token;
+    })
+};
+
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+    
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    })
+};
+
+UserSchema.statics.findByToken = function(token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch(e) {
+        return Promise.reject();
+    }
+    
+    // findOne returns a promise
+    // keys wrapped in quotes (on the bottom 2) because they are nested, first for consistency
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
+/**
+ * returns a promise to accept or reject an email and password combination
+ */
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            // bcrypt.compare to compare pass and user.pass
+            // if the result is true = call resolve with user
+            // if the result is false = reject
+        
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){
+                    resolve(user);
+                }else{
+                    // console.log('time to reject')
+                    // reject('User and password combination not found.');
+                    Promise.reject('User and pass combination not found.')
+                }
+            });
+        });
+    });
+};
+
+UserSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else{
+        next();
+    }
+});
+
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,123 +1,51 @@
 'use strict';
+require('./config/config');
 
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
 
 const express = require('express');
-const socketIO = require('socket.io');
+const session = require('express-session');
+
+// const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
 
-const {Users} = require('./utils/users');
-const {Games} = require('./utils/games');
-
+// LOCAL
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
 
 const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../views');
 const port = process.env.PORT || 3000;
 var app = express();
 
-var server = http.createServer(app);
-var io = socketIO(server);
-var users = new Users();
-var games = new Games();
+var server = http.createServer(app); 
 
 // MIDDLEWARE
 app.use(express.static(publicPath));
 
 // figure out which i actually need
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-hbs.registerPartials(__dirname + './../views/partials');
+app.use( bodyParser.json() );                               // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded( {extended: true} ));         // to support URL-encoded bodies
+// hbs.registerPartials(__dirname + './../views/partials');
+hbs.registerPartials(path.join(__dirname, './../views/partials'));
+
 app.set('view engine', 'hbs');
 
-io.on('connection', (socket) => {
-    console.log('new user connected');
+/// SESSION STUFFS
 
-    app.post('/game', (req, res) => {
-        // res.sendFile('./game.php');
-        
-        var name = req.body.name;
-        var game = req.body.game;
-        var role = req.body.role;
-        
-        // make request to items.json
-        fs.readFile(__dirname + './../public/data/items-base.json','utf8', (err, items) => {
-            if(err) throw err;
-            
-            var items = JSON.parse(items).data;
-            var data = {name, game, role, items};
-            if(role === 'gameMaster') {
-                res.render('game_master.hbs', {data});
-            } else if(role === 'player') {
-                res.render('game_player.hbs', {data});
-            } else {
-                // bad info, handle somehow
-                res.send('bad info');
-            }
-        });
-        // app.set('x-name', name);
-        // app.set('x-game', game);
-        // app.set('x-role', role);
-    });
-
-    socket.on('joinLobby', (callback) => {
-        socket.join('lobby');
-        socket.game = 'lobby';
-        
-        // emit rooms to user in lobby
-        socket.emit('updateGamesList', JSON.stringify(games));
-        callback();
-    });
-
-    socket.on('joinGame', (params, callback) => {
-        // add authentication here
-        
-        // leave previous game sockets (or lobby)
-        // will this still exist????
-        if(socket.game){
-            socket.leave(socket.game);
-            var game = games.removeUserFromGame(socket.game);
-        }
-        
-        socket.join(params.game);
-        socket.game = params.game;
-
-        users.removeUser(socket.id);
-        users.addUser(socket.id, params.name, params.game, params.role);
-        console.log('user added');
-
-        
-        games.addUserToGame(params.game);
-
-        // first thing not used yet
-        io.to(params.game).emit('updateUserList', users.getUserList(params.game));
-        io.to('lobby').emit('updateGamesList', JSON.stringify(games));
-        console.log('rooms/games: ', socket.adapter.rooms);
-        
-        socket.broadcast.to(params.game).emit('welcome', {name: params.name, role: params.role});
-        console.log(`user entered ${params.game}`);
-        callback();
-    });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        var user = users.removeUser(socket.id);
-
-        if(user) {
-            var game = games.removeUserFromGame(user.game);
-            
-            io.to('lobby').emit('updateGamesList', JSON.stringify(games));
-
-            io.to(user.game).emit('updateUserList', users.getUserList(user.game));
-            // io.to(user.game).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
-            console.log(`user left ${user.game}`);
-        }
-    });
-});
-
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
 
 server.listen(port, () => {
     console.log(`Started on port ${port}`);
